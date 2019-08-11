6a13ad532646cad73b449c030a06fbc74546dc34
deanrivers <deanriversdeveloper@gmail.com>
2019-08-11 13:58:31 -0400

    git ignore test

--- ApexStatsBot.py
+++ ApexStatsBot.py
@@ -10,6 +10,8 @@ from requests.auth import HTTPBasicAuth
 import datetime
 import sys
 
+#slight change
+
 #twitter validation
 auth = tweepy.OAuthHandler(cfg.consumer_key, cfg.consumer_secret)
 auth.set_access_token(cfg.access_token, cfg.access_token_secret)
