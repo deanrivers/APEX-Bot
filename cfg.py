ae9d54297f1cebbd7e5f3d5f2a84b0ff43f03113
Dean Russell Rivers <39504530+deanrivers@users.noreply.github.com>
2019-08-11 13:54:06 -0400

    Delete cfg.py

--- cfg.py
+++
@@ -1,6 +0,0 @@
-consumer_key = 's90KhQh0fDvTyKDJAjt7CZIny'
-consumer_secret = 'EQway9lJhv9XKuxmtoFBP0FFyrU6B9vSljrgK0vvijxGdePb7t'
-access_token = '1150495036116819970-vK60Tve1nkXeXwT3fRJHzi4TJ5qAiF'
-access_token_secret = 'AZn9gkwl3NTQ09Jn7Vp5LL6P5NBXhLMvweV4NxvqZ0GvC'
-
-APEX_KEY = '3c0a15fd-ac7c-4f37-9f7a-47bd7a76286d'
