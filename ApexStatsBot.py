6c7d664d77056f08831d30ab5209ba42031dd856
deanrivers <deanriversdeveloper@gmail.com>
2019-09-06 20:39:03 -0400

    Update

--- ApexStatsBot.py
+++ ApexStatsBot.py
@@ -37,6 +37,7 @@ def updateSinceIDs(tweetID):
     f = open("since_ids.txt", "a")
     f.write(str(tweetID) + "\n") 
     f.close()
+    print 'Update triggered for: ' + str(tweetID)
 
     #open and read the file after the appending:
     # f = open("since_ids.txt", "r")
@@ -50,7 +51,7 @@ def retrieveLastID():
     fileHandle = open ( 'since_ids.txt',"r" )
     lineList = fileHandle.readlines()
     fileHandle.close()
-    print lineList
+    #print lineList
     print "The last line value in the since_ids.txt is: "
     #print lineList[len(lineList)-1]
     # or simply
@@ -96,8 +97,9 @@ def process():
     for idx, tweet in enumerate(tweets):
         time.sleep(1)
 
-        #update since_id and append to text file for every tweet    
-        since_id = str(tweets[idx]['id_str'])    
+        #update batchLastId and append to text file for every tweet    
+        since_id = str(tweets[0]['id_str'])
+           
         
 
          #declare date and time for all possible tweets to use
@@ -128,12 +130,14 @@ def process():
 
         #split further by colon
         newSplit = incomingTweet[1].split(":")
+        print 'new'
         print newSplit
 
         #apply platfrom and username
 
-        if len(incomingTweet) >2:
+        if len(incomingTweet) >2 and len(newSplit) >= 2:
             platform = newSplit[0]
+            print incomingTweet
             username = newSplit[1] + '%20'+  incomingTweet[2]
 
         elif len(incomingTweet) >=2 and len(newSplit) >= 2:
@@ -143,8 +147,6 @@ def process():
         else:
             platform = 'xbl'
             username = 'Invalid Username'
-
-        
             
         print (platform)
         print (username)
@@ -230,9 +232,9 @@ def process():
                 print postTweet
                 print '-----------Posting Tweet-----------'
                 #print postTweet
-                #api.update_status(postTweet)
+                api.update_status(postTweet)
                 print '-----------Tweet Posted-----------'
-                since_id = str(tweets[idx]['id_str'])
+                #since_id = str(tweets[idx]['id_str'])
                 
                 
 
@@ -243,29 +245,30 @@ def process():
                 print ''
                 postTweet = 'Hey @'+ screen_name + ", nothing found for '" + username + "' on '" +platform+ "'"+'.' '\n'+dateDay+' '+dateTime
                 print postTweet
-                since_id = str(tweets[idx]['id_str'])
+                #since_id = str(tweets[idx]['id_str'])
                 
                 
                 time.sleep(1)
                 
             else: 
                 print 'The APEX API could not be accessed for some reason.'
-                since_id = str(tweets[idx]['id_str'])
+                #since_id = str(tweets[idx]['id_str'])
                 
         else: 
             print 'Bad info.'
-            since_id = str(tweets[idx]['id_str'])
+            #since_id = str(tweets[idx]['id_str'])
             
             
             # postTweet = 'Hey @'+ screen_name + ', the information you provided was blasphemous. Please follow the guidelines when using this bot. I was created by a human!'+'\n'+dateDay+' '+dateTime
-            
-        updateSinceIDs(since_id)    
+        #since_id = str(tweets[idx]['id_str'])    
+        
             
     if len(tweets) == 0:
         print 'nothing new on initial start'
         since_id = last_id
     else:
-        print 'This is the since_id before while loop: ' + since_id    
+        print 'This is the last_id before the while loop: ' + last_id 
+        updateSinceIDs(str(tweets[0]['id_str']))
 
     #enter While loop section
     ############################################################################################################################################################################################################
@@ -276,7 +279,8 @@ def process():
     
 
 
-
+    print '-----------While Loop Start-----------'
+    print ' '  
 
     while 1>0:
         time.sleep(1)
@@ -285,11 +289,16 @@ def process():
         x = datetime.datetime.now()
         dateDay = x.strftime("%x")
         dateTime = x.strftime("%X")
+        batchLastId = retrieveLastID()
+
+        
 
         print '-----------While Loop Iteration Start-----------'
         print ' '        
             
-        twitter_endpoint = 'https://api.twitter.com/1.1/statuses/mentions_timeline.json?screen_name=ApexLegendsBot' + '&since_id=' + since_id
+        twitter_endpoint = 'https://api.twitter.com/1.1/statuses/mentions_timeline.json?screen_name=ApexLegendsBot' + '&since_id=' + batchLastId
+        
+        # print twitter_endpoint
 
         twitter_response, twitter_data = client.request(twitter_endpoint)
 
@@ -311,10 +320,12 @@ def process():
                 print 'Tweet: ' + text
                 print 'string ID: ' + str(str_ID)
 
-            for tweet in tweets:
+            for idx, tweet in enumerate(tweets):
 
                 text = tweet['text']
                 screen_name = tweet['user']['screen_name']
+                since_id = str(tweets[idx]['id_str'])
+                batchLastId = str(tweets[0]['id_str'])
 
                 #for loop through mentions and lookup API
                 #1. look up info...if nothing retrieved do nothing
@@ -335,7 +346,7 @@ def process():
 
                 #apply platfrom and username
 
-                if len(incomingTweet) >2:
+                if len(incomingTweet) >2 and len(newSplit) >= 2:
                     platform = newSplit[0]
                     username = newSplit[1] + '%20'+  incomingTweet[2]
 
@@ -425,45 +436,41 @@ def process():
                         #post tweet
                         print postTweet
                         print '-----------Posting Tweet tweeted-----------' 
-                        #api.update_status(postTweet)
+                        api.update_status(postTweet)
                         print '-----------Tweet Posted-----------' 
                         
                     
                         #set new max id to the first entry to come in
-                        since_id = str(tweets[0]['id_str'])
-                        print 'This is the since_id for this tweet: ' + since_id 
+                        #since_id = str(tweets[idx]['id_str'])
+                        print 'This is the since_id that this tweet replied to: ' + since_id 
                         
 
                     #nothing returned from API
                     elif statusCode == 400:
                         #platform = splitTweet[1]
                         print 'There is some bad info provided'
-                        print ''
+                        print 'nothing will be posted'
                         postTweet = 'Hey @'+ screen_name + ", nothing found for '" + username + "' on '" +platform+ "'"+'.' '\n'+dateDay+' '+dateTime
                         print postTweet
-                        print '-----------Posting Tweet not actually tweeted-----------' 
-                        
-                        print '-----------Tweet Posted not actually tweeted-----------' 
-                        since_id = str(tweets[0]['id_str'])
-                        
+                        #since_id = str(tweets[idx]['id_str'])
                         time.sleep(1)
                         
                         
                     else: 
                         print 'The APEX API could not be accessed for some reason.'
-                        since_id = str(tweets[0]['id_str'])
-                        
+                        #since_id = str(tweets[idx]['id_str'])                        
                         time.sleep(1)
                 else: 
                     print 'Bad info.'
                     #postTweet = 'Hey @'+ screen_name + ', the information you provided was blasphemous. Please follow the guidelines when using this bot. I was created by a human!'+'\n'+dateDay+' '+dateTime
                     #print postTweet
-                    since_id = str(tweets[0]['id_str'])
+                    #since_id = str(tweets[idx]['id_str'])
                     
 
                 time.sleep(1)
-            
-            updateSinceIDs(since_id)
+
+                
+            updateSinceIDs(batchLastId)
 
         elif len(tweets) == 0:
             
